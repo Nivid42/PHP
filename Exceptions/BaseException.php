874558942a927c14ed2251d31b031b829c96eaf8
Nivid42 <169085077+Nivid42@users.noreply.github.com>
2025-09-29 22:28:42 +0200

    PHP-1 Initial commit BaseException.php
    Commit of the commented Version of BaseException.php

---
+++ Exceptions/BaseException.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * BaseException class which inherits from Exception
+ * Goal of it is to inherit from this for others
+ */
+class BaseException extends \Exception
+{ 
+    protected ?int $httpCode;
+    protected ?string $errorCode;
+    
+    /**
+     * Constructor to initialize values.
+     *
+     * @param string $message The error message
+     * @param int $code Optional internal code
+     * @param int|null $httpCode Optional HTTP code, default null
+     * @param string|null $errorCode Optional custom error code, default null
+     * @param Throwable|null $previous Optional previous exception, default null
+     */
+
+    public function __construct(
+        string $message,
+        int $code = 0,
+        ?int $httpCode = null,
+        ?string $errorCode = null,
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+        $this->httpCode = $httpCode;
+        $this->errorCode = $errorCode;
+    }
+        
+    /**
+     * Get the HTTP code, or null if not set
+     *
+     * @return int|null
+     */
+    public function getHttpCode(): ?int
+    {
+        return $this->httpCode; // returns NULL if not set
+    }
+    
+    /**
+     * Get the Error code, or null if not set
+     *
+     * @return string|null
+     */
+    public function getErrorCode(): ?string
+    {
+        return $this->errorCode;
+    }
+    
+    /**
+     * Converts Exception into a structured Array
+     *
+     * @return array
+     */
+    public function toArray():array
+    {
+        return [
+            'message' => $this->getMessage(),
+            'code' => $this->getCode(),
+            'errorCode' => $this->getErrorCode(),
+            'httpCode' => $this->getHttpCode(),
+        ];
+    }
+}
